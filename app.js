a658151a2a53146874d3b66b2f4b0bd12ba49928
Matías Martearena <maticorvooficial@gmail.com>
2023-07-18 10:48:03 -0300

    create(global): create digital movies's project

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const PORT = 3000
+const publicPath = path.resolve(__dirname, './public')
+
+app.use(express.static(publicPath))
+app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
+app.get('/', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
