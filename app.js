335ba591918f424ac16302744f5828a775d2e0e3
Matías Martearena <maticorvooficial@gmail.com>
2023-08-01 11:57:21 -0300

    feat(home): navbar

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
 app.get('/', (req, res) => {
-   res.sendFile(path.resolve(__dirname, './views/home-page/home.html'))
+   res.sendFile(path.resolve(__dirname, './views/home/home.html'))
 })
