f28a98d41923f5fe4541e6fbf778ce5b03cf2479
Pablo Bejarano <pablobejarano2112@gmail.com>
2023-08-04 18:46:08 -0300

    feature(login) added animated background

--- app.js
+++ app.js
@@ -10,3 +10,6 @@ app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, './views/home-page/home.html'))
 })
+app.get('/login', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/login/login.html'))
+})
