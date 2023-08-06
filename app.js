60bd29ef00f6a49f13acfaf976022f230dcbd2a7
Luciano Ayrala <Luciano.12Ayrala@gmail.com>
2023-08-06 19:55:24 -0300

    fix(sign-in): Correcciones html y css

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
-const PORT = 3000
+const path = require('path')
 const publicPath = path.resolve(__dirname, './public')
 
 app.use(express.static(publicPath))
-app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
+
+const port = process.env.PORT || 3001;
+app.listen( port, () => console.log(`Server up:  PORT:${port}`) );
 
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, './views/sign-in/sign-in.html'))
+})
+
+app.get('/sign-in', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/sign-in/sign-in.html'))
+})
+
+app.post('/sign-in',  (req,  res) =>{
+   res.redirect('/');
+})
+
+app.get('/login', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/login/login.html'))
+})
+
+app.post('/login',  (req,  res) =>{
+   res.redirect('/');
 })
