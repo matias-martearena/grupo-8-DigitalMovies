1490fb050dda35b8250e808fce394be687fb9361
Matías Martearena <maticorvooficial@gmail.com>
2023-08-18 10:32:13 -0300

    feat(membership): add controller and routes about membership page

---
+++ src/controllers/membership.js
@@ -0,0 +1,7 @@
+const membership = {
+   membership: (req, res) => {
+      res.render('products/membership/membership')
+   },
+}
+
+module.exports = membership
--- src/routes/main.js
+++ src/routes/main.js
@@ -4,9 +4,11 @@ const router = express.Router()
 // Requerimiento de controladores
 const homePage = require('../controllers/home')
 const cartPage = require('../controllers/cart')
+const membershipPage = require('../controllers/membership')
 
 // Routes
 router.get('/', homePage.home)
 router.get('/cart', cartPage.cart)
+router.get('/membership', membershipPage.membership)
 
 module.exports = router
