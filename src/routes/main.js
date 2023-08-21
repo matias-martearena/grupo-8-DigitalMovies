7b5eeca71970522a9ba4c42bbdbef69b3e2a468c
TitiBeguier <beguiercartalibro@gmail.com>
2023-08-21 14:46:15 -0300

    modified:   public/styles/showtimes/showtimes.css

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,10 +5,13 @@ const router = express.Router()
 const homePage = require('../controllers/home')
 const cartPage = require('../controllers/cart')
 const membershipPage = require('../controllers/membership')
+const showtimesPage = require('../controllers/showtimes')
 
 // Routes
 router.get('/', homePage.home)
 router.get('/cart', cartPage.cart)
 router.get('/membership', membershipPage.membership)
+router.get('/showtimes', showtimesPage.showtimes)
+router.get('/tickets', showtimesPage.showtimes)
 
 module.exports = router
