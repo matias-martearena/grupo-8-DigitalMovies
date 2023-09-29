1740f66b868b69c977640e449744d560a431a107
Pablo Bejarano <pablobejarano2112@gmail.com>
2023-09-28 23:19:08 -0300

    feature(register-crud) added post route for register page and hash in password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,38 @@
+const fs = require('fs')
+const path = require('path')
+const bcrypt = require('bcryptjs')
+
 const trailerVideos = require('../database/trailers.json')
 const arrCartProducts = require('../database/cartProducts.json')
+
+const usersFilePath = path.join(
+   __dirname,
+   '..',
+   'database',
+   'users.json'
+)
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
 
 const userController = {
    login: (req, res) => {
       res.render('users/login', {
          trailers: trailerVideos,
       })
+   },
+   store: (req, res) => {
+      const newId = 'USR' + Date.now()
+      const newImgName = '/images' + req.file?.filename
+      const newUser = {
+         email: req.body.email,
+         first_name: req.body.first_name,
+         id: newId,
+         image: newImgName,
+         last_name: req.body.last_name,
+         password: bcrypt.hashSync(req.body.password, 10)
+      }
+      users.push(newUser)
+      fs.writeFileSync(usersFilePath, JSON.stringify(users))
+      res.redirect('/')
    },
    cart: (req, res) => {
       res.render('users/cart', {
