ec356e3819f74c9290a1c51c497146c07c93aac1
Matías Martearena <maticorvooficial@gmail.com>
2023-10-05 12:59:41 -0300

    feat(register): validation data user on register form

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,46 +1,58 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('node:fs')
+const path = require('node:path')
 const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
 
 const trailerVideos = require('../database/trailers.json')
 const arrCartProducts = require('../database/cartProducts.json')
 
-const usersFilePath = path.join(
-   __dirname,
-   '..',
-   'database',
-   'users.json'
-)
+const usersFilePath = path.join(__dirname, '..', 'database', 'users.json')
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'))
 
 const userController = {
+   register: (req, res) => {
+      res.render('users/register')
+   },
+
+   processRegister: (req, res) => {
+      const resultValidation = validationResult(req)
+
+      if (resultValidation.errors.length > 0) {
+         return res.render('users/register', {
+            errors: resultValidation.mapped(),
+            oldData: req.body,
+         })
+      }
+
+      const newId = 'USR' + Date.now()
+      const newImg = `/images/users/${req.file?.filename}`
+
+      const newUser = {
+         email: req.body.email,
+         first_name: req.body.first_name,
+         id: newId,
+         image: newImg,
+         last_name: req.body.last_name,
+         password: bcrypt.hashSync(req.body.password, 10),
+      }
+
+      users.push(newUser)
+
+      fs.writeFileSync(usersFilePath, JSON.stringify(users))
+
+      return res.redirect('/')
+   },
+
    login: (req, res) => {
       res.render('users/login', {
          trailers: trailerVideos,
       })
    },
-   store: (req, res) => {
-      const newId = 'USR' + Date.now()
-      const newImgName = '/images' + req.file?.filename
-      const newUser = {
-         email: req.body.email,
-         first_name: req.body.first_name,
-         id: newId,
-         image: newImgName,
-         last_name: req.body.last_name,
-         password: bcrypt.hashSync(req.body.password, 10)
-      }
-      users.push(newUser)
-      fs.writeFileSync(usersFilePath, JSON.stringify(users))
-      res.redirect('/')
-   },
+
    cart: (req, res) => {
       res.render('users/cart', {
          cartProducts: arrCartProducts,
       })
-   },
-   register: (req, res) => {
-      res.render('users/register')
    },
 }
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,26 +1,42 @@
 const express = require('express')
 const multer = require('multer')
-const path = require('path')
+const path = require('node:path')
 const router = express.Router()
 
+// ---------- Controllers ---------- //
 const userController = require('../controllers/userController')
 
+// ---------- Middlewares ---------- //
+const registrationValidation = require('../middlewares/validationRegister')
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-       const pathImage = path.join(__dirname, '..', '..', 'public', 'images')
-       cb(null, pathImage)
-    },
-    filename: (req, file, cb) => {
-       const newFileName = '/img-' + Date.now() + path.extname(file.originalname)
-       cb(null, newFileName)
-    },
- })
+   destination: (req, file, cb) => {
+      const pathImage = path.join(
+         __dirname,
+         '..',
+         '..',
+         'public',
+         'images',
+         'users'
+      )
+      cb(null, pathImage)
+   },
+   filename: (req, file, cb) => {
+      const newFileName = `img-${Date.now()}${path.extname(file.originalname)}`
+      cb(null, newFileName)
+   },
+})
 
 const upload = multer({ storage: storage })
 
 router.get('/login', userController.login)
 router.get('/cart', userController.cart)
 router.get('/register', userController.register)
-router.post('/', upload.single('image'), userController.store)
+router.post(
+   '/register',
+   upload.single('image'),
+   registrationValidation,
+   userController.processRegister
+)
 
 module.exports = router
