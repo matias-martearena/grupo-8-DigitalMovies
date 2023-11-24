44ecd84a4d6fa90fd5bfc360015bb5c0ccac0342
Matías Martearena <maticorvooficial@gmail.com>
2023-11-24 19:08:19 -0300

    feat(snack): add backend validations to snacks forms

--- src/controllers/snacksController.js
+++ src/controllers/snacksController.js
@@ -1,59 +1,86 @@
 const db = require('../database/models')
+const { validationResult } = require('express-validator')
 
 const snacksController = {
+   // Snacks page
    snacksIndex: (req, res) => {
-      db.Snack.findAll()
-      .then((snacks) => {
+      db.Snack.findAll().then(snacks => {
          res.render('products/snacks', {
             snacksProducts: snacks,
          })
       })
    },
 
+   // Create a new snack
    snacksCreate: (req, res) => {
       res.render('products/snacks-create-form')
    },
-
    snacksStore: (req, res) => {
+      const resultValidation = validationResult(req)
+
+      if (resultValidation.errors.length > 0) {
+         return res.render('products/snacks-create-form', {
+            errors: resultValidation.mapped(),
+            oldData: req.body,
+         })
+      }
+
       db.Snack.create({
-         image: req.body.image,
+         image: req.file.filename,
          description: req.body.description,
          price: req.body.price,
+      }).then(function () {
+         res.redirect('/snacks')
       })
-      res.redirect('/')
    },
 
+   // Edit or update a snack
    snacksEdit: (req, res) => {
       const { id } = req.params
-      db.Snack.findByPk(id)
-      .then((prod)=>{
+      db.Snack.findByPk(id).then(prod => {
          res.render('products/snacks-edit-form', {
             productToEdit: prod,
          })
       })
    },
-
    snacksUpdate: (req, res) => {
-      db.Snack.update({
-         image: req.body.image,
-         description: req.body.description,
-         price: req.body.price,
-      }, {
-         where: {
-            id: req.params.id
-         }
-      })
+      const resultValidation = validationResult(req)
 
-      res.redirect('/')
+      if (resultValidation.errors.length > 0) {
+         const { id } = req.params
+         db.Snack.findByPk(id).then(prod => {
+            res.render('products/snacks-edit-form', {
+               productToEdit: prod,
+               errors: resultValidation.mapped(),
+            })
+         })
+      }
+
+      db.Snack.update(
+         {
+            image: req.file?.filename || req.body.image,
+            description: req.body.description,
+            price: req.body.price,
+         },
+         {
+            where: {
+               id: req.params.id,
+            },
+         }
+      ).then(function () {
+         res.redirect('/snacks')
+      })
    },
 
+   // Delete a snack
    snacksDestroy: (req, res) => {
       db.Snack.destroy({
          where: {
             id: req.params.id,
-         }
+         },
+      }).then(function () {
+         res.redirect('/snacks')
       })
-      res.redirect('/')
    },
 }
 
--- src/routes/snacks.js
+++ src/routes/snacks.js
@@ -6,12 +6,30 @@ const snacksController = require('../controllers/snacksController')
 
 // ---------- Middlewares ---------- //
 const upload = require('../middlewares/snackMulterMiddleware')
+const snackValidationData = require('../middlewares/validationSnack')
 
+// ---------- /snacks Snack Page ---------- //
 router.get('/', snacksController.snacksIndex)
+
+// ---------- Create a new snack ---------- //
 router.get('/create', snacksController.snacksCreate)
-router.post('/', upload.single('image'), snacksController.snacksStore)
+router.post(
+   '/create',
+   upload.single('image'),
+   snackValidationData,
+   snacksController.snacksStore
+)
+
+// ---------- Edit or update a snack ---------- //
 router.get('/edit/:id', snacksController.snacksEdit)
-router.put('/edit/:id', upload.single('image'), snacksController.snacksUpdate)
+router.put(
+   '/edit/:id',
+   upload.single('image'),
+   snackValidationData,
+   snacksController.snacksUpdate
+)
+
+// ---------- Delete a snack ---------- //
 router.delete('/delete/:id', snacksController.snacksDestroy)
 
 module.exports = router
