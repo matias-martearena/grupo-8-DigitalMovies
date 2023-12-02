eddc649b5e33731a9ab182f6af34bae1d897c0ee
Pablo Bejarano <pablobejarano2112@gmail.com>
2023-12-02 18:13:38 -0300

    (add(filter) added filter for movies and series in the home page

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,6 +6,9 @@ const mainController = require('../controllers/mainController')
 
 // Routes
 router.get('/', mainController.home)
+router.get('/movies', mainController.movies)
+router.get('/series', mainController.series)
+router.get('/best-rating', mainController.ratingFilter)
 router.get('/products/:id', mainController.detail)
 router.get('/manage', mainController.editor)
 
